5c7d25927c448a02137d7b6775ff3ed50d038236
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-08-10 19:00:30 +0530

    Implementted better solution for two sum

--- problems/array/TwoSum.java
+++ problems/array/TwoSum.java
@@ -1,13 +1,14 @@
 package problems.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 // https://leetcode.com/problems/two-sum/
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums = { 3, 2, 4 };
-        int target = 6;
-        int[] result = twoSumBrute(nums, target);
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = twoSumBetter(nums, target);
         System.out.println(Arrays.toString(result));
     }
 
@@ -21,6 +22,22 @@ public class TwoSum {
                     result[1] = j;
                     return result;
                 }
+            }
+        }
+        return result;
+    }
+
+    static int[] twoSumBetter(int[] a, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            int key = target - a[i];
+            if (map.containsKey(key)) {
+                result[0] = map.get(key);
+                result[1] = i;
+                return result;
+            } else {
+                map.put(a[i], i);
             }
         }
         return result;
