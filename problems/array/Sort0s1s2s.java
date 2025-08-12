fa605c784f0a856c51d1fd1a6d82f0ceeb71cefc
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-08-12 08:15:56 +0530

    Implemented optimal solution for 0's, 1's and 2's sorting problem using Dutch national flag algo

--- problems/array/Sort0s1s2s.java
+++ problems/array/Sort0s1s2s.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Sort0s1s2s {
     public static void main(String[] args) {
         int[] nums = { 0, 1, 0, 0, 2, 1, 0, 2, 1, 2, 0, 1, 2, 1 };
-        int[] result = sortBetter(nums);
+        int[] result = sortOptimal(nums);
         System.out.println(Arrays.toString(result));
     }
 
@@ -15,7 +15,7 @@ public class Sort0s1s2s {
         return nums;
     }
 
-    // o(n+n+n) = o(n)
+    // o(n+n+n) = o(3n)
     static int[] sortBetter(int[] nums) {
         int count0 = 0, count1 = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -36,5 +36,33 @@ public class Sort0s1s2s {
         }
 
         return nums;
+    }
+
+    // Dutch national flag algo->[Red, White, Blue];
+    // Low for => 0;
+    // Hight for => 2;
+    // Mid for current element;
+    static int[] sortOptimal(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+        return nums;
+    }
+
+    static void swap(int[] nums, int start, int end) {
+        int temp = nums[end];
+        nums[end] = nums[start];
+        nums[start] = temp;
     }
 }
