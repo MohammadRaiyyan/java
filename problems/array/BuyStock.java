6c50abdfc51f5c4b0222e150aec65261a82553eb
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-08-13 22:22:28 +0530

    Refactor BuyStock class to use optimal method for maximum profit calculation

--- problems/array/BuyStock.java
+++ problems/array/BuyStock.java
@@ -3,7 +3,7 @@ package problems.array;
 public class BuyStock {
     public static void main(String[] args) {
         int[] stockPrices = { 7, 1, 5, 3, 6, 4 };
-        int maxProfit = maxProfitBrute(stockPrices);
+        int maxProfit = maxProfitOptimal(stockPrices);
         System.out.println(maxProfit);
     }
 
@@ -23,16 +23,10 @@ public class BuyStock {
         int maxProfit = 0;
         int minPrice = prices[0];
         for (int i = 1; i < prices.length; i++) {
-            if (minPrice > prices[i]) {
-                int profit = prices[i] - minPrice;
-                minPrice = prices[i];
-                maxProfit = Math.max(maxProfit, profit);
-            } else {
-                int profit = prices[i] - minPrice;
-                maxProfit = Math.max(maxProfit, profit);
-            }
+            int cost = prices[i] - minPrice;
+            minPrice = Math.min(prices[i], minPrice);
+            maxProfit = Math.max(maxProfit, cost);
         }
-
         return maxProfit;
     }
 }
