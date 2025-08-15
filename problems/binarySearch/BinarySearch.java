d2021d938bca0090b950810a3b88795591fc436d
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-08-16 00:27:34 +0530

    Refactor BinarySearch class to use recursive search method and update target value in main method

--- problems/binarySearch/BinarySearch.java
+++ problems/binarySearch/BinarySearch.java
@@ -3,8 +3,9 @@ package problems.binarySearch;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = { -1, 0, 3, 5, 9, 12 };
-        int target = 9;
-        int result = search(nums, target);
+        int target = 0;
+        // int result = search(nums, target);
+        int result = searchRecursive(nums, target, 0, nums.length - 1);
         System.out.println(result);
     }
 
@@ -13,7 +14,7 @@ public class BinarySearch {
         int end = nums.length - 1;
 
         while (start <= end) {
-            int mid = (int) (start + end) / 2;
+            int mid = (start + end) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (target > nums[mid]) {
@@ -23,5 +24,20 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+
+    static int searchRecursive(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (target > nums[mid]) {
+            return searchRecursive(nums, target, mid + 1, end);
+        }
+        return searchRecursive(nums, target, start, mid - 1);
     }
 }
