b26801e7f6447bc52292595417442803ee7fa995
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-08-16 10:18:55 +0530

    Implement UpperBound class for binary search to find the smallest number greater than the target value

---
+++ problems/binarySearch/UpperBound.java
@@ -0,0 +1,28 @@
+package problems.binarySearch;
+
+public class UpperBound {
+    public static void main(String[] args) {
+        int[] nums = { 1, 4, 6, 7, 9, 10, 12, 13, 14, 17, 18 };
+        int target = 7;
+        int result = search(nums, target);
+        System.out.println(result);
+    }
+
+    // find number smallest grater to target
+    static int search(int[] nums, int target) {
+        int ans = nums.length;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] > target) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
