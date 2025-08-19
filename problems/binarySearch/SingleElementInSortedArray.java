e62e4d6f8872782b444ef345d44ed0ed42731043
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-08-19 09:08:20 +0530

    Refactor main method to use singleNonDuplicate for finding the single element in a sorted array

--- problems/binarySearch/SingleElementInSortedArray.java
+++ problems/binarySearch/SingleElementInSortedArray.java
@@ -3,7 +3,7 @@ package problems.binarySearch;
 public class SingleElementInSortedArray {
     public static void main(String[] args) {
         int[] nums = { 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6 };
-        int result = find(nums);
+        int result = singleNonDuplicate(nums);
         System.out.println(result);
 
     }
@@ -33,6 +33,43 @@ public class SingleElementInSortedArray {
                 }
             }
 
+        }
+
+        return -1;
+    }
+
+    public static int singleNonDuplicate(int[] arr) {
+        int start = 1, end = arr.length - 2;
+        // If only one element return that as single element
+        if (arr.length == 1)
+            return arr[0];
+        // If first element is not equal to start element then first element is single
+        // element
+        if (arr[0] != arr[start]) {
+            return arr[0];
+        }
+        // If last element is not equal to end element then its a single single element
+        if (arr[arr.length - 1] != arr[end]) {
+            return arr[arr.length - 1];
+        }
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            // If previous and next element is not same which is on mid then its a single
+            // element
+            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) {
+                return arr[mid];
+            } // 0 1 2 3 4 5 6 7
+              // (even,odd) : [even,odd,even,odd,even,odd,even,odd]
+              // If mid is odd and previous element equal to mid then eliminate left part
+              // OR
+              // If mid is even and next element equal to mid then eliminate left part
+
+            if ((mid % 2 == 1 && arr[mid - 1] == arr[mid]) || (mid % 2 == 0 && arr[mid + 1] == arr[mid])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
 
         return -1;
